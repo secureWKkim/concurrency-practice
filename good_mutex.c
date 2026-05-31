421dc723de842a363efe4561974b43d7ebe12e15
secureWKkim <1rladnjsrud1@gmail.com>
2026-05-31 15:18:58 +0900

    [fix] compile error resolved

--- good_mutex.c
+++ good_mutex.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdbool.h>
 /* CAS는 세마포어, 락프리, 웨이트프리한 데이터 구조를 구현하기 위해 이용하는 처리다.  아토믹하지 않다.
 아토믹하게 처리하려면 내장 함수인 _sync_bool_compare_and_swap(same signature as below) 를 사용한다. */
 bool compare_and_swap(uint64_t* p, uint64_t oldval, uint64_t newval) {
--- pthread_mutex.c
+++ pthread_mutex.c
@@ -18,7 +18,7 @@ void* func_for_thread(void* arg) {
     return NULL;
 }
 
-int main (int argc, char* argv[]) {
+int pthread_mutex_example(int argc, char* argv[]) {
     //스레드 생성
     pthread_t th1, th2;
     if (pthread_create(&th1, NULL, func_for_thread, NULL) != 0) {
